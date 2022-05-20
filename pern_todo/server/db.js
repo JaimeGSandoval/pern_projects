947313e4917203e5befa94565f8a98e3ec24f41a
jaimegsandoval <jaime.devs@gmail.com>
2022-05-19 23:40:45 -0700

    update the db config to use a connection string

--- pern_todo/server/db.js
+++ pern_todo/server/db.js
@@ -1,12 +1,16 @@
 require('dotenv/config');
 const Pool = require('pg').Pool;
 
+// const pool = new Pool({
+//   user: process.env.USER,
+//   password: process.env.POSTGRES_PASS,
+//   host: process.env.HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DATABASE,
+// });
+
 const pool = new Pool({
-  user: process.env.USER,
-  password: process.env.POSTGRES_PASS,
-  host: process.env.HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DATABASE,
+  connectionString: process.env.CONNECTION_STRING,
 });
 
 module.exports = pool;
